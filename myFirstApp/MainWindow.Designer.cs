c019233b9d654ad92ef7bdb6272880ba0017c0a9
Alec Tecson <tecsonalr@gmail.com>
2021-06-15 19:59:31 +0800

    Textbox Control (Getting, Setting, Validating & Regex Implementation)

--- myFirstApp/MainWindow.Designer.cs
+++ myFirstApp/MainWindow.Designer.cs
@@ -30,48 +30,120 @@ namespace myFirstApp
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.num1Text = new System.Windows.Forms.TextBox();
+            this.num2Text = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.resultText = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.num1ErrorLabel = new System.Windows.Forms.Label();
+            this.num2ErrorLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(447, 148);
+            this.label1.Location = new System.Drawing.Point(131, 124);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.Size = new System.Drawing.Size(63, 15);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Number 1";
+            // 
+            // num1Text
+            // 
+            this.num1Text.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.num1Text.Location = new System.Drawing.Point(134, 143);
+            this.num1Text.MaxLength = 10;
+            this.num1Text.Name = "num1Text";
+            this.num1Text.Size = new System.Drawing.Size(225, 23);
+            this.num1Text.TabIndex = 1;
+            this.num1Text.TextChanged += new System.EventHandler(this.num1Text_TextChanged);
+            // 
+            // num2Text
+            // 
+            this.num2Text.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.num2Text.Location = new System.Drawing.Point(134, 188);
+            this.num2Text.MaxLength = 10;
+            this.num2Text.Name = "num2Text";
+            this.num2Text.Size = new System.Drawing.Size(225, 23);
+            this.num2Text.TabIndex = 3;
+            this.num2Text.TextChanged += new System.EventHandler(this.num2Text_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(447, 177);
+            this.label2.Location = new System.Drawing.Point(131, 169);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(42, 15);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Number 2";
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(284, 217);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(75, 23);
+            this.addBtn.TabIndex = 4;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // resultText
+            // 
+            this.resultText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.resultText.Location = new System.Drawing.Point(134, 263);
+            this.resultText.Name = "resultText";
+            this.resultText.Size = new System.Drawing.Size(225, 23);
+            this.resultText.TabIndex = 6;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.ForeColor = System.Drawing.Color.Maroon;
-            this.label3.Location = new System.Drawing.Point(447, 206);
+            this.label3.Location = new System.Drawing.Point(131, 244);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(42, 15);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Result";
+            // 
+            // num1ErrorLabel
+            // 
+            this.num1ErrorLabel.AutoSize = true;
+            this.num1ErrorLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num1ErrorLabel.Location = new System.Drawing.Point(337, 118);
+            this.num1ErrorLabel.Name = "num1ErrorLabel";
+            this.num1ErrorLabel.Size = new System.Drawing.Size(29, 37);
+            this.num1ErrorLabel.TabIndex = 7;
+            this.num1ErrorLabel.Text = "*";
+            this.num1ErrorLabel.Visible = false;
+            // 
+            // num2ErrorLabel
+            // 
+            this.num2ErrorLabel.AutoSize = true;
+            this.num2ErrorLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num2ErrorLabel.Location = new System.Drawing.Point(337, 164);
+            this.num2ErrorLabel.Name = "num2ErrorLabel";
+            this.num2ErrorLabel.Size = new System.Drawing.Size(29, 37);
+            this.num2ErrorLabel.TabIndex = 8;
+            this.num2ErrorLabel.Text = "*";
+            this.num2ErrorLabel.Visible = false;
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(933, 519);
+            this.Controls.Add(this.resultText);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.num2Text);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.num1Text);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.num1ErrorLabel);
+            this.Controls.Add(this.num2ErrorLabel);
             this.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Font = new System.Drawing.Font("Segoe UI", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Maroon;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -86,8 +158,14 @@ namespace myFirstApp
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox num1Text;
+        private System.Windows.Forms.TextBox num2Text;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.TextBox resultText;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label num1ErrorLabel;
+        private System.Windows.Forms.Label num2ErrorLabel;
     }
 }
 
--- myFirstApp/MainWindow.cs
+++ myFirstApp/MainWindow.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace myFirstApp
 {
@@ -15,6 +16,72 @@ namespace myFirstApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            Regex rg = new Regex("^[0-9]+$");
+
+            if (num1Text.Text == "")
+            {
+                num1ErrorLabel.Visible = true;
+            }
+            else
+            {
+                num1ErrorLabel.Visible = false;
+            }
+
+            if (num2Text.Text == "")
+            {
+                num2ErrorLabel.Visible = true;
+            }
+            else
+            {
+                num2ErrorLabel.Visible = false;
+            }
+
+            if (num1ErrorLabel.Visible || num2ErrorLabel.Visible)
+            {
+                MessageBox.Show("Fields with * are mandatory.");
+            }
+            else
+            {
+                if (rg.Match(num1Text.Text).Success && rg.Match(num2Text.Text).Success)
+                {
+                    int num1 = Convert.ToInt32(num1Text.Text);
+                    int num2 = Convert.ToInt32(num2Text.Text);
+                    int result = num1 + num2;
+                    resultText.Text = result.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Only numerics are accepted.");
+                }
+            }
+        }
+
+        private void num1Text_TextChanged(object sender, EventArgs e)
+        {
+            if (num1Text.Text == "")
+            {
+                num1ErrorLabel.Visible = true;
+            }
+            else
+            {
+                num1ErrorLabel.Visible = false;
+            }
+        }
+
+        private void num2Text_TextChanged(object sender, EventArgs e)
+        {
+            if (num2Text.Text == "")
+            {
+                num2ErrorLabel.Visible = true;
+            }
+            else
+            {
+                num2ErrorLabel.Visible = false;
+            }
         }
     }
 }
